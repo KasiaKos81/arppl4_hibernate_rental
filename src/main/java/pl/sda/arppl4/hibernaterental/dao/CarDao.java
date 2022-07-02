5f789f8b1da9f45787ef05b2d8b2c2449045758c
Kasia <kasiakos81@gmail.com>
2022-07-02 12:26:49 +0200

    020722

--- src/main/java/pl/sda/arppl4/hibernaterental/dao/CarDao.java
+++ src/main/java/pl/sda/arppl4/hibernaterental/dao/CarDao.java
@@ -5,7 +5,31 @@ import pl.sda.arppl4.hibernaterental.model.Car;
 import java.util.List;
 import java.util.Optional;
 
-public class CarDao {
+public class CarDao implements ICarDao{
 
 
+    @Override
+    public void addsCar(Car car) {
+
+    }
+
+    @Override
+    public void removeCar(Car car) {
+
+    }
+
+    @Override
+    public Optional<Car> listOneCar(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Car> listeAllCars() {
+        return null;
+    }
+
+    @Override
+    public void updateCar(Car car) {
+
+    }
 }
